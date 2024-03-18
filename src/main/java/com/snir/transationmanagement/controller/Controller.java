26338a70df46088a5d9ba6478bbff7aab8e28bfd
snir-maatuf <122987145+snir-maatuf@users.noreply.github.com>
2024-03-18 17:52:11 +0200

    add CRUD functionality

--- src/main/java/com/snir/transationmanagement/controller/Controller.java
+++ src/main/java/com/snir/transationmanagement/controller/Controller.java
@@ -3,10 +3,15 @@ package com.snir.transationmanagement.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import com.snir.transationmanagement.model.Employee;
 import com.snir.transationmanagement.services.EmployeeServices;
@@ -23,4 +28,40 @@ public class Controller {
 		Employee employeeSavedToDB = this.employeeServices.addEmployee(employee);
 		return new ResponseEntity<Employee>(employeeSavedToDB, HttpStatus.CREATED);
 	}
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = this.employeeServices.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+	@GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") int id) {
+        Employee employee = this.employeeServices.getEmployeeById(id);
+        if (employee != null) {
+            return new ResponseEntity<>(employee, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+	@PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee) {
+        Employee updatedEmployee = this.employeeServices.updateEmployee(id, employee);
+        if (updatedEmployee != null) {
+            return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+	@DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable("id") int id) {
+        boolean deleted = this.employeeServices.deleteEmployee(id);
+        if (deleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/snir/transationmanagement/services/EmployeeServices.java
+++ src/main/java/com/snir/transationmanagement/services/EmployeeServices.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.snir.transationmanagement.model.Address;
 import com.snir.transationmanagement.model.Employee;
 import com.snir.transationmanagement.repository.EmployeeRepository;
+import java.util.List;
 
 @Service
 public class EmployeeServices {
@@ -16,6 +17,7 @@ public class EmployeeServices {
 	@Autowired
 	private AddressServices addressService;
 	
+
 	@Transactional
 	public Employee addEmployee(Employee employee) throws Exception {
 		Employee employeeSavedToDB = this.employeeRepository.save(employee);
@@ -29,4 +31,35 @@ public class EmployeeServices {
 		this.addressService.addAddress(address);
 		return employeeSavedToDB;
 	}
+
+	public List<Employee> getAllEmployees() {
+        return this.employeeRepository.findAll();
+    }
+    
+    public Employee getEmployeeById(int id) {
+        return this.employeeRepository.findById(id).orElse(null);
+    }
+
+	@Transactional
+    public Employee updateEmployee(int id, Employee updatedEmployee) {
+        Employee existingEmployee = this.employeeRepository.findById(id).orElse(null);
+        if (existingEmployee != null) {
+            // Update the existing employee's information
+            existingEmployee.setName(updatedEmployee.getName());
+            // Save the updated employee
+            return this.employeeRepository.save(existingEmployee);
+        }
+        return null;
+    }
+    
+    @Transactional
+    public boolean deleteEmployee(int id) {
+        Employee existingEmployee = this.employeeRepository.findById(id).orElse(null);
+        if (existingEmployee != null) {
+            // Delete the employee
+            this.employeeRepository.delete(existingEmployee);
+            return true;
+        }
+        return false;
+    }
 }
